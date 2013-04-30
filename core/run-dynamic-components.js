92bbd6fdc090ea7bdd30080564bf339b09fea8f6
Eugene Krevenets <hyzhak@ya.ru>
2013-05-01 00:53:08 +0200

    compare in dynamic components experiment v0.0.3 and v0.0.4

--- core/run-dynamic-components.js
+++ core/run-dynamic-components.js
@@ -3,74 +3,166 @@
  * Copyright (c) 2013, Eugene-Krevenets
  */
 
-(function(darlingjs) {
+(function() {
     'use strict';
 
-    var m = darlingjs.module('systemPerformance');
-    var COUNT = 100;
+    var world_0_0_3,
+        world_0_0_4;
 
-    m.$c('system1');
-    m.$c('system2');
+    world_0_0_3 = (function(darlingjs, darlingutil) {
 
-    m.$s('system1', {
-        $require: ['system1'],
+        var m = darlingjs.module('systemPerformance');
+        var COUNT = 100;
 
-        enabled: true,
+        m.$c('system1');
+        m.$c('system2');
 
-        $beforeUpdate: ['$world', '$time', '$nodes', function($world, $time, $nodes) {
-            if ($nodes.length() <= 0) {
-                system1.enabled = false;
-                system2.enabled = true;
-            } else {
-                if (this.enabled) {
-                    this.targetIndex = Math.floor($nodes.length() * Math.random());
+        m.$s('system1', {
+            $require: ['system1'],
+
+            enabled: true,
+
+            $beforeUpdate: ['$world', '$time', '$nodes', function($world, $time, $nodes) {
+                if ($nodes.length() <= 0) {
+                    system1.enabled = false;
+                    system2.enabled = true;
+                } else {
+                    if (this.enabled) {
+                        this.targetIndex = Math.floor($nodes.length() * Math.random());
+                    }
                 }
-            }
-        }],
+            }],
 
-        $update: ['$node', function($node) {
-            if(this.enabled && --this.targetIndex === -1) {
-                $node.$remove('system1');
-                $node.$add('system2');
-            }
-        }]
-    });
-
-    m.$s('system2', {
-        $require: ['system2'],
-
-        enabled: false,
-
-        $beforeUpdate: ['$world', '$time', '$nodes', function($world, $time, $nodes) {
-            if ($nodes.length() <= 0) {
-                system1.enabled = true;
-                system2.enabled = false;
-            } else {
-                if (this.enabled) {
-                    this.targetIndex = Math.floor($nodes.length() * Math.random());
+            $update: ['$node', function($node) {
+                if(this.enabled && --this.targetIndex === -1) {
+                    $node.$remove('system1');
+                    $node.$add('system2');
                 }
-            }
-        }],
+            }]
+        });
 
-        $update: ['$node', function($node) {
-            if(this.enabled && --this.targetIndex === -1) {
-                $node.$remove('system2');
-                $node.$add('system1');
-            }
-        }]
-    });
+        m.$s('system2', {
+            $require: ['system2'],
 
-    var world = darlingjs.world('performance-empty', ['systemPerformance', 'ngPerformance']);
-    world.$add('ngPerformanceBefore');
-    var system1 = world.$add('system1');
-    var system2 = world.$add('system2');
-    world.$add('ngPerformanceAfter');
+            enabled: false,
 
-    var components = ['system1'];
-    for(var i = 0; i < COUNT; i++) {
-        world.$add(world.$e(components));
-    }
+            $beforeUpdate: ['$world', '$time', '$nodes', function($world, $time, $nodes) {
+                if ($nodes.length() <= 0) {
+                    system1.enabled = true;
+                    system2.enabled = false;
+                } else {
+                    if (this.enabled) {
+                        this.targetIndex = Math.floor($nodes.length() * Math.random());
+                    }
+                }
+            }],
 
-    world.$start();
+            $update: ['$node', function($node) {
+                if(this.enabled && --this.targetIndex === -1) {
+                    $node.$remove('system2');
+                    $node.$add('system1');
+                }
+            }]
+        });
 
-})(darlingjs);
+        var world = darlingjs.world('performance-empty', ['systemPerformance']);
+        var system1 = world.$add('system1');
+        var system2 = world.$add('system2');
+
+        var components = ['system1'];
+        for(var i = 0; i < COUNT; i++) {
+            world.$add(world.$e(components));
+        }
+
+        return world;
+    })(darlingjs_0_0_3, darlingutil_0_0_3);
+
+    world_0_0_4 = (function(darlingjs, darlingutil) {
+
+        var m = darlingjs.module('systemPerformance');
+        var COUNT = 100;
+
+        m.$c('system1');
+        m.$c('system2');
+
+        m.$s('system1', {
+            $require: ['system1'],
+
+            enabled: true,
+
+            $beforeUpdate: ['$world', '$time', '$nodes', function($world, $time, $nodes) {
+                if ($nodes.length() <= 0) {
+                    system1.enabled = false;
+                    system2.enabled = true;
+                } else {
+                    if (this.enabled) {
+                        this.targetIndex = Math.floor($nodes.length() * Math.random());
+                    }
+                }
+            }],
+
+            $update: ['$node', function($node) {
+                if(this.enabled && --this.targetIndex === -1) {
+                    $node.$remove('system1');
+                    $node.$add('system2');
+                }
+            }]
+        });
+
+        m.$s('system2', {
+            $require: ['system2'],
+
+            enabled: false,
+
+            $beforeUpdate: ['$world', '$time', '$nodes', function($world, $time, $nodes) {
+                if ($nodes.length() <= 0) {
+                    system1.enabled = true;
+                    system2.enabled = false;
+                } else {
+                    if (this.enabled) {
+                        this.targetIndex = Math.floor($nodes.length() * Math.random());
+                    }
+                }
+            }],
+
+            $update: ['$node', function($node) {
+                if(this.enabled && --this.targetIndex === -1) {
+                    $node.$remove('system2');
+                    $node.$add('system1');
+                }
+            }]
+        });
+
+        var world = darlingjs.world('performance-empty', ['systemPerformance']);
+        var system1 = world.$add('system1');
+        var system2 = world.$add('system2');
+
+        var components = ['system1'];
+        for(var i = 0; i < COUNT; i++) {
+            world.$add(world.$e(components));
+        }
+
+        return world;
+    })(darlingjs_0_0_4, darlingutil_0_0_4);
+
+
+    //Run tests
+
+    var suite = new Benchmark.Suite;
+
+    suite.add('$update in ver 0.0.3 (call or apply)', function() {
+        world_0_0_3.$update(0.1);
+    })
+    .add('$update in ver 0.0.4 (member method)', function() {
+        world_0_0_4.$update(0.1);
+    })
+    // add listeners
+    .on('cycle', function(event) {
+        console.log(String(event.target));
+    })
+    .on('complete', function() {
+        console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+    })
+    // run async
+    .run({ 'async': true });
+})();
