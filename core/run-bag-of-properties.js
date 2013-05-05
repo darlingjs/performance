77c1541524e9df1ae3ac034344ef4d8c7ebc2e16
Eugene Krevenets <hyzhak@ya.ru>
2013-05-05 16:25:33 +0200

    test of preparing components of entity

--- core/run-bag-of-properties.js
+++ core/run-bag-of-properties.js
@@ -6,115 +6,96 @@
     'use strict';
 
     //Run tests
-    var ENTITY_COUNT = 1000,
-        COMPONENTS_COUNT = 10;
+var ENTITY_COUNT = 100,
+    COMPONENTS_COUNT = 10;
 
-    function buildEntity() {
-        return {
-            $name: 'entity'
-        };
-    }
+function buildEntity() {
+    return {
+        $name: 'entity'
+    };
+}                                                          g
 
-    function buildEntities(entityCount, componentCount, value) {
-        var result = [];
-        while(--entityCount>=0) {
-            var entity = buildEntity();
-            for(var i = 0; i < componentCount; i++) {
-                buildComponent(entity, i, value);
-                if (getComponent(entity, i)) {
-                    getComponent(entity, i).count = 0;
-                }
+function buildEntities(entityCount, componentCount, value) {
+    var result = [];
+    while(--entityCount>=0) {
+        var entity = buildEntity();
+        for(var i = 0; i < componentCount; i++) {
+            buildComponent(entity, i, value);
+            if (getComponentByComponentIndexDirect(entity, i)) {
+                getComponentByComponentIndexDirect(entity, i).count = 0;
             }
-            result.push(entity);
         }
-
-        return result;
+        result.push(entity);
     }
 
-    var emptyEntites = buildEntities(ENTITY_COUNT, 0),
-        preparedByObjectEntities = buildEntities(ENTITY_COUNT, COMPONENTS_COUNT, {}),
-        preparedByNullEntities = buildEntities(ENTITY_COUNT, COMPONENTS_COUNT, null);
+    return result;
+}
 
-    function getRandomComponentOfRandomEntityAndIncreaseOfCount(entites) {
-        var entity = entites[Math.floor(ENTITY_COUNT * Math.random())];
-        var componentIndex = Math.floor(COMPONENTS_COUNT * Math.random());
-        if(!getComponent(entity, componentIndex)) {
-            buildComponent(entity, componentIndex, {});
-            getComponent(entity, componentIndex).count = 0;
-        }
+var emptyEntities = buildEntities(ENTITY_COUNT, 0),
+    preparedByObjectEntities = buildEntities(ENTITY_COUNT, COMPONENTS_COUNT, {}),
+    preparedByNullEntities = buildEntities(ENTITY_COUNT, COMPONENTS_COUNT, null);
 
-        getComponent(entity, componentIndex).count++;
+function getRandomComponentOfRandomEntityAndIncreaseOfCount(entites, getComponentByComponentIndex) {
+    var entity = entites[Math.floor(ENTITY_COUNT * Math.random())];
+    var componentIndex = Math.floor(COMPONENTS_COUNT * Math.random());
+    if(!getComponentByComponentIndex(entity, componentIndex)) {
+        buildComponent(entity, componentIndex, {});
+        getComponentByComponentIndex(entity, componentIndex).count = 0;
     }
 
-    function getComponent(entity, componentIndex) {
-        switch(componentIndex) {
-            case 0:
-                return entity.ngComponent0;
-            case 1:
-                return entity.ngComponent1;
-            case 2:
-                return entity.ngComponent2;
-            case 3:
-                return entity.ngComponent3;
-            case 4:
-                return entity.ngComponent4;
-            case 5:
-                return entity.ngComponent5;
-            case 6:
-                return entity.ngComponent6;
-            case 7:
-                return entity.ngComponent7;
-            case 8:
-                return entity.ngComponent8;
-            case 9:
-                return entity.ngComponent9;
-        }
-    }
+    getComponentByComponentIndex(entity, componentIndex).count++;
+}
 
-    function buildComponent(entity, componentIndex, value) {
-        switch(componentIndex) {
-            case 0:
-                entity.ngComponent0 = value;
-                break;
-            case 1:
-                entity.ngComponent1 = value;
-                break;
-            case 2:
-                entity.ngComponent2 = value;
-                break;
-            case 3:
-                entity.ngComponent3 = value;
-                break;
-            case 4:
-                entity.ngComponent4 = value;
-                break;
-            case 5:
-                entity.ngComponent5 = value;
-                break;
-            case 6:
-                entity.ngComponent6 = value;
-                break;
-            case 7:
-                entity.ngComponent7 = value;
-                break;
-            case 8:
-                entity.ngComponent8 = value;
-                break;
-            case 9:
-                entity.ngComponent9 = value;
-                break;
-        }
+function getComponentByComponentIndexDirect(entity, componentIndex) {
+    switch(componentIndex) {
+        case 0:
+            return entity.ngComponent0;
+        case 1:
+            return entity.ngComponent1;
+        case 2:
+            return entity.ngComponent2;
+        case 3:
+            return entity.ngComponent3;
+        case 4:
+            return entity.ngComponent4;
+        case 5:
+            return entity.ngComponent5;
+        case 6:
+            return entity.ngComponent6;
+        case 7:
+            return entity.ngComponent7;
+        case 8:
+            return entity.ngComponent8;
+        case 9:
+            return entity.ngComponent9;
+        default:
+            throw new Error('out of range');
     }
+}
+
+function getComponentByComponentIndexDynamic(entity, componentIndex) {
+    return entity['ngComponent' + componentIndex];
+}
+
+function buildComponent(entity, componentIndex, value) {
+    entity['ngComponent' + componentIndex] = value;
+}
 
     var suite = new Benchmark.Suite();
-    suite.add('entity with prepared by object components', function() {
-        getRandomComponentOfRandomEntityAndIncreaseOfCount(preparedByObjectEntities);
+    suite.add('entity with prepared by object components (direct access)', function() {
+        getRandomComponentOfRandomEntityAndIncreaseOfCount(preparedByObjectEntities, getComponentByComponentIndexDirect);
     })
-    .add('entity with prepared by null components', function() {
-        getRandomComponentOfRandomEntityAndIncreaseOfCount(preparedByNullEntities);
+    .add('entity with prepared by null components (direct access)', function() {
+        getRandomComponentOfRandomEntityAndIncreaseOfCount(preparedByNullEntities, getComponentByComponentIndexDirect);
     })
-    .add('empty entities', function() {
-        getRandomComponentOfRandomEntityAndIncreaseOfCount(emptyEntites);
+    .add('entity with prepared by null components (dynamic access)', function() {
+        getRandomComponentOfRandomEntityAndIncreaseOfCount(preparedByNullEntities, getComponentByComponentIndexDynamic);
+    })
+    .add('empty entities (direct access)', function() {
+        getRandomComponentOfRandomEntityAndIncreaseOfCount(emptyEntities, getComponentByComponentIndexDirect);
+    })
+    .add('empty entities (dynamic access)', function() {
+        getRandomComponentOfRandomEntityAndIncreaseOfCount(emptyEntities, getComponentByComponentIndexDynamic);
     })
     // add listeners
     .on('cycle', function(event) {
